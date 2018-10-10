e20df8fd9d500d7e24403c2afbb76b4eac5e69ff
Sairam <majgaonkars3@gmail.com>
2018-10-10 13:49:22 +0000

    Havoc OS-Related Changes

--- device.mk
+++ device.mk
@@ -431,3 +431,5 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(DEVICE_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+
+export export HAVOC_BUILD_TYPE=Official

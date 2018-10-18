ed51a695daea5f43e33194b39f6a5ebfb38aeb37
SaksHam1714 <Saks.Ham1714@gmail.com>
2018-10-18 12:53:16 +0000

    Xtended Bring-up & Fixed Typo

--- xtended_land.mk
+++ xtended_land.mk
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2016 The CyanogenMod Project
-#               2017 The LineageOS Project
+#               2017 The XtendedOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -21,7 +21,7 @@ DEVICE_PATH := device/xiaomi/land
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff
+# Inherit some common Xtended stuff
 $(call inherit-product, vendor/xtended/config/common_full_phone.mk)
 
 # Inherit from land device
